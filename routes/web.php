836466b122713506ecfa712d48a8d54ac65cf8ca
mrizkimaulidan <mrizkimaulidan@icloud.com>
2025-12-05 16:52:39 +0800

    added default layout

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\CoffeeController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', DashboardController::class)->name('dashboard');
+
+Route::get('/kopi', [CoffeeController::class, 'index'])->name('coffees.index');
