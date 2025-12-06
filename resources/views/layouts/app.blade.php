2ef90a06b363ad021ed917438773997d4b33ec28
mrizkimaulidan <mrizkimaulidan@icloud.com>
2025-12-06 19:52:45 +0800

    ui: :lipstick: added icon each menu on navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,10 +20,18 @@
       </button>
       <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div class="navbar-nav w-100">
-          <a class="nav-link active" href="{{ route('dashboard') }}">Home</a>
-          <a class="nav-link" href="{{ route('kopi.index') }}">Daftar Kopi</a>
-          <a class="nav-link" href="{{ route('kriteria.index') }}">Daftar Kriteria</a>
-          <a class="nav-link" href="{{ route('pengguna.index') }}">Daftar Pengguna</a>
+          <a class="nav-link active" href="{{ route('dashboard') }}">
+            <i class="bi bi-speedometer2 me-1"></i> Dashboard
+          </a>
+          <a class="nav-link" href="{{ route('kopi.index') }}">
+            <i class="bi bi-cup-straw me-1"></i> Daftar Kopi
+          </a>
+          <a class="nav-link" href="{{ route('kriteria.index') }}">
+            <i class="bi bi-clipboard-data me-1"></i> Daftar Kriteria
+          </a>
+          <a class="nav-link" href="{{ route('pengguna.index') }}">
+            <i class="bi bi-person-badge me-1"></i> Daftar Pengguna
+          </a>
 
           <!-- This will push to right -->
           <div class="nav-link ms-auto">
