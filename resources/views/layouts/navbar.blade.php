6e4a8720367c7d62c57e954b8c54403b0883d3e0
mrizkimaulidan <mrizkimaulidan@icloud.com>
2025-12-10 03:09:50 +0800

    feat: :sparkles: added active state on navbar menu

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -7,19 +7,20 @@
     </button>
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
       <div class="navbar-nav w-100">
-        <a class="nav-link active" href="{{ route('dashboard') }}">
+        <a class="nav-link {{ request()->routeIs('dashboard') ? 'active' : '' }}" href="{{ route('dashboard') }}">
           <i class="bi bi-speedometer2 me-1"></i> Dashboard
         </a>
-        <a class="nav-link" href="{{ route('kopi.index') }}">
+        <a class="nav-link {{ request()->routeIs('kopi.*') ? 'active' : '' }}" href="{{ route('kopi.index') }}">
           <i class="bi bi-cup-straw me-1"></i> Daftar Kopi
         </a>
-        <a class="nav-link" href="{{ route('kriteria.index') }}">
+        <a class="nav-link {{ request()->routeIs('kriteria.*') ? 'active' : '' }}" href="{{ route('kriteria.index') }}">
           <i class="bi bi-clipboard-data me-1"></i> Daftar Kriteria
         </a>
-        <a class="nav-link" href="{{ route('pengguna.index') }}">
+        <a class="nav-link {{ request()->routeIs('pengguna.*') ? 'active' : '' }}" href="{{ route('pengguna.index') }}">
           <i class="bi bi-person-badge me-1"></i> Daftar Pengguna
         </a>
-        <a class="nav-link" href="{{ route('pertanyaan.index') }}">
+        <a class="nav-link {{ request()->routeIs('pertanyaan.*') ? 'active' : '' }}"
+          href="{{ route('pertanyaan.index') }}">
           <i class="bi bi-question-circle me-1"></i> Daftar Pertanyaan
         </a>
 
