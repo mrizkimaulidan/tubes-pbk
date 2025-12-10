67c11de569ca892bfec26198bf218daa63c1c1c9
Nurussyifaa <nurussyifaa3@gmail.com>
2025-12-10 21:13:47 +0800

    modified model SurveyQuetionOption

--- app/Models/SurveyQuestionOption.php
+++ app/Models/SurveyQuestionOption.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class SurveyQuestionOption extends Model
 {
-    //
+    public function SurveyQuetion()
+    {
+        return $this->belongsTo(SurveyQuestion::class);
+    }
 }
